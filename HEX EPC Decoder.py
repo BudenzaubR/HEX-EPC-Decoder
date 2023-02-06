ae2d7505df2621b8d8364bd4e709dbee56aa6b7a
BudenzaubR <bastian.blankemeier@gmail.com>
2023-02-06 21:42:35 +0100

    tkinter changes => open csv file

--- HEX EPC Decoder.py
+++ HEX EPC Decoder.py
@@ -7,47 +7,13 @@ from tkinter import simpledialog
 from tkinter import messagebox
 from epcutils import binary2epctaguri, epcpureidentityuri2gs1element, epctaguri2epcpureidentityuri, hex2binary
 
-##### MAIN PROGRAMM #####
-### User Input ###
-# Open main window
-root = Tk()
-root.geometry("300x200")
-root.resizable(False,False)
-root.title("EPC HEX Decoder")
-
-# Main Frame
-mainframe = ttk.Frame(root, padding=10)
-mainframe.grid()
-
-# Select CSV File
-csvlabelframe = ttk.LabelFrame(mainframe, text="CSV").grid(row=0, column=0, sticky=(E, W),columnspan=2)
-
-
-
-ttk.Button(mainframe, text="Quit", command=root.destroy).grid(column=1, row=0)
-root.mainloop()
-
-# Input CSV File Path
-#filename = askopenfilename(title="Select CSV File") # show an "Open" dialog box and return the path to the selected file
-filename = "hex.csv"
-
-#hasheaders = messagebox.askyesno("CSV Headers", "Does the CSV-File contain headers?")
-hasheaders = True
-
-# Input CSV Delimiter
-#delimiter = simpledialog.askstring("CSV Delimiter", "Which delimiter is used?")
-delimiter = ','
-
-# Input HEX EPC Column
-#column = simpledialog.askinteger("EPC Column", "In which column are the Hex EPCs?")
-column = 1
-
+########## MAIN PROGRAMM ##########
 ### Start of conversion ###
 # Process CSV to convert HEX EPCs to all other formats
 # Output is written to results.csv
-def convert():        
+def convert(inputfile):        
     try:        
-        with open(filename) as csvdatei:
+        with open(inputfile) as csvdatei:
             csvReader = csv.reader(csvdatei, delimiter=delimiter)
             with open("results.csv",'w', newline='') as outputFile:
                     csvWriter = csv.writer(outputFile, delimiter=delimiter)
@@ -85,6 +51,47 @@ def convert():
                     messagebox.showinfo(message="HEX EPC Conversion complete!")
     except:
         messagebox.showerror(title = "Excpetion", message="An error occurred, please check your input values and retry!")
-        
 
-        
+# Function to open the CSV file
+def open_file():
+    filename.set(askopenfilename(title="Select CSV File"))
+
+### User Input ###
+# Open main window
+root = Tk()
+root.geometry("600x400")
+root.resizable(False,False)
+root.title("EPC HEX Decoder")
+
+# Main Frame
+mainframe = ttk.Frame(root, padding=10)
+mainframe.grid()
+
+# Select CSV File
+csvlabelframe = ttk.LabelFrame(mainframe, text="CSV").grid(row=0, column=0, sticky=(E, W),columnspan=2)
+filename = StringVar()
+csventry = ttk.Entry(csvlabelframe, textvariable=filename, state=DISABLED)
+csventry.grid(row=0, column=0)
+
+
+csvopendialogbutton = ttk.Button(csvlabelframe, text="Select File", command=open_file)
+csvopendialogbutton.grid(row=0, column=1)
+
+# Exit Button
+ttk.Button(mainframe, text="Exit", command=root.destroy).grid(row=2, column=0)
+root.mainloop()
+
+# Input CSV File Path
+#filename = askopenfilename(title="Select CSV File") # show an "Open" dialog box and return the path to the selected file
+#filename = "hex.csv"
+
+#hasheaders = messagebox.askyesno("CSV Headers", "Does the CSV-File contain headers?")
+hasheaders = True
+
+# Input CSV Delimiter
+#delimiter = simpledialog.askstring("CSV Delimiter", "Which delimiter is used?")
+delimiter = ','
+
+# Input HEX EPC Column
+#column = simpledialog.askinteger("EPC Column", "In which column are the Hex EPCs?")
+column = 1
